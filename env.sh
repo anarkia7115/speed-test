32ea868fc095de62e40f2d69ae411d63649be1d6
gaojx <gaojxcs@gmail.com>
2019-10-16 10:45:17 +0800

    separate env

---
+++ env.sh
@@ -0,0 +1,1 @@
+export BIGFILE="./read.go"
--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
-export BIGFILE="./read.go"
+#!/bin/bash
+. env.sh
 echo 3 | sudo tee /proc/sys/vm/drop_caches
 time go run read.go 1000000000 2000000000 10 
 time ./a.out
